37e5d1e72c87b846d054653ee0dbd874ff5f3291
a_katsay a_katsay <a_katsay@mail.ru>
2023-05-16 11:39:47 -0400

    basic setup ready

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { MainLayout } from '@/Layouts/MainLayout'
+import { NextPage } from 'next'
+import Link from 'next/link'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <MainLayout title="Home">
+        
+      </MainLayout>
+    </>
+  )
+}
+
+export default Home
