8ef025b4b6816fb0a06472199e47999f15d88618
a_katsay a_katsay <a_katsay@mail.ru>
2023-05-22 23:10:57 -0400

    almossst there

--- server/app.js
+++ server/app.js
@@ -3,18 +3,19 @@ const axios = require('axios')
 const express = require('express')
 const multer = require('multer')
 const cors = require('cors')
+const fs = require('fs')
 
 const app = express()
-const upload = multer()
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors())
 
 app.post('/api/transcribe', upload.single('file'), async (req, res) => {
 
+  const fileStream = fs.createReadStream(req.file.path)
   const formData = new FormData()
-  formData.append('file', req.file)
+  formData.append('file',fileStream, req.file.originalname)
   formData.append('model', 'whisper-1')
-  formData.append('language', 'en')
 
   console.log(req.file)
   console.log(formData)
@@ -34,27 +35,47 @@ app.post('/api/transcribe', upload.single('file'), async (req, res) => {
     res.status(500).json({ msg: `error sending request to openai: ${e.message}` })
   }
 
-  // try {
-
-  //   const res = await fetch('https://api.openai.com/v1/audio/transcriptions', {
-  //     headers: {
-  //       Authorization: `Bearer ${process.env.API}`,
-  //     },
-  //     method: 'POST',
-  //     body: formData,
-  //   })
-
-  //   const data = await res.json()
-
-  //   res.json(data)
-
-  // } catch (e) {
-  //   console.log(e.message)
-  //   res.status(500).json({msg: `error sending request to openai: ${e.message}`})
-  // }
-
 })
 
 app.listen('5000', () => {
   console.log('app is running on port 5000')
 })
+
+
+
+
+
+// to bypass library //
+
+// const response = await axios.post('https://api.openai.com/v1/audio/transcriptions', formData, {
+//   headers: {
+//     'Authorization': `Bearer ${process.env.API}`,
+//     'Content-type': 'multipart/form-data'
+//   }
+// })
+
+// res.json(response.data)
+
+
+
+
+// fetch option //
+
+// try {
+
+//   const res = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+//     headers: {
+//       Authorization: `Bearer ${process.env.API}`,
+//     },
+//     method: 'POST',
+//     body: formData,
+//   })
+
+//   const data = await res.json()
+
+//   res.json(data)
+
+// } catch (e) {
+//   console.log(e.message)
+//   res.status(500).json({msg: `error sending request to openai: ${e.message}`})
+// }
